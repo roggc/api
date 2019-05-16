27730d283d09c15879ce8463b69ddfcfb71aad03
roger gc <nppcpp@hotmail.com>
2019-05-16 19:05:14 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -14,6 +14,26 @@ const port = process.env.PORT||3000
 app.use(cors())
 app.options('*', cors())
 
+app.use
+(
+  (req, res, next) =>
+  {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader
+    (
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    res.setHeader
+    (
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    )
+    res.setHeader('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin')
+    next()
+  }
+)
+
 const start=async()=>
 {
   const db=await mongo.connect(uri,{useNewUrlParser:true}).then(cli=>cli.db(name))
