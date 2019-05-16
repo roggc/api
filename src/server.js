efe3d83420ef6efd3f5d8c6f5cbe4861a95ac467
roger gc <nppcpp@hotmail.com>
2019-05-16 19:54:05 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -20,33 +20,48 @@ const start=async()=>
     resolvers,
     context:({req})=>({db,req})
   })
-
-    app.use(cors())
+  app.use
+  (
+    (req, res, next) =>
+    {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader
+      (
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      )
+      res.setHeader
+      (
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+      )
+      res.setHeader('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin')
+      next()
+    }
+  )
   server.applyMiddleware({app,path:'/',cors:false})
-  // app.use
-  // (
-  //   (req, res, next) =>
-  //   {
-  //     res.setHeader("Access-Control-Allow-Origin", "*")
-  //     res.setHeader
-  //     (
-  //       "Access-Control-Allow-Headers",
-  //       "Origin, X-Requested-With, Content-Type, Accept"
-  //     )
-  //     res.setHeader
-  //     (
-  //       "Access-Control-Allow-Methods",
-  //       "GET, POST, PATCH, DELETE, OPTIONS"
-  //     )
-  //     res.setHeader('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin')
-  //     next()
-  //   }
-  // )
+  app.use
+  (
+    (req, res, next) =>
+    {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader
+      (
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      )
+      res.setHeader
+      (
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+      )
+      res.setHeader('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin')
+      next()
+    }
+  )
   // app.use(cors())
   // app.options('*', cors())
 
-  app.use(cors())
-  // app.options('*', cors())
   app.listen(port,()=>console.log(`Now browse to localhost:${port}${server.graphqlPath}`))
 }
 
