f14fa05f8cf851a4617d4e6b79543a407849a8d5
roger gc <nppcpp@hotmail.com>
2019-05-21 12:47:25 +0200

    cookie

--- src/root.js
+++ src/root.js
@@ -155,6 +155,7 @@ export default
             {
               httpOnly: true,
               maxAge: 1000 * 60 * 60 * 24 * 31,
+              path:"/logsignin"
             })
             out=
             {
